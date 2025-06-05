105518fed2632833847ec442d677f15390ef941f
Ciloe <ciloe@hotmail.fr>
2025-06-05 14:18:17 +0200

    Add DateRange
    Signed-off-by: Ciloe <ciloe@hotmail.fr>

--- src/DateRange.php
+++ src/DateRange.php
@@ -9,6 +9,7 @@ use Ciloe\Ranges\Exception\InvalidBoundException;
 use Ciloe\Ranges\Exception\InvalidDateIntervalException;
 use Ciloe\Ranges\Exception\InvalidInfiniteBoundException;
 use DateInterval;
+use DateMalformedStringException;
 use DateTime;
 use DateTimeImmutable;
 use DateTimeInterface;
@@ -40,6 +41,9 @@ class DateRange implements RangeInterface
         return $this->lowerBound . $lowerValue . ',' . $upperValue . $this->upperBound;
     }
 
+    /**
+     * @throws DateMalformedStringException
+     */
     public static function fromString(string $range): self
     {
         if (
